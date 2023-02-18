84febdbc39fef7f96fc823017a6861d8aa1c77c2
Mariyan Maksimov <mariyan.pickup@gmail.com>
2023-02-18 16:55:26 +0200

    init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	// "fmt"
+	// "log"
+	// "encoding/json"
+	// "math/rand"
+	// "net/http"
+	// "strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	Firstname string `json:firstname`
+	Lastname string `json:lastname`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+}
