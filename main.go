091147710fa834844f2ae275b1e8202f5479ce57
Mariyan Maksimov <mariyan.pickup@gmail.com>
2023-02-19 14:31:55 +0200

    added del movie

--- main.go
+++ main.go
@@ -40,8 +40,16 @@ func updateMovie()  {
 	
 }
 
-func deleteMovie()  {
-	
+func deleteMovie(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index:+1]...)
+		}
+	}
 }
 
 func main() {
